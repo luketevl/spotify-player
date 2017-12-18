bef92c7fe519257e61c6507b44538732078ad0c1
luketevl <luketevl@gmail.com>
2017-12-18 11:05:21 -0200

    finished player

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,5 +1,5 @@
 import SpotifyWrapper from 'spotify-wrapper';
 const spotify = new SpotifyWrapper({
-  token: 'BQBoXYqT0bG3PGiIUj9MAvF6-GYXwU_TxcTutpGkIkWnvJD5NN9KEaZIiazjRr0eKAEV09tarj563mmh6DKWOIId2Yk-tsSAfAiwZpHD30RFflZPMOZe_RDRUDdTlqCyCUd_aK46Qcg'
+  token: 'BQB6vH0dg8oI39wl4POA09LaiqaZ1kCTVSjq_-pmfVug50lxzB_Zu7KkISHrt02lFcJFga9BZkHwelNfqelFnNMvNBKHY3MycSqa7R9VL9LiAgKwUMvRZtC85kdAI7y37zmYSkyZeRk'
   });
 export default spotify;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import spotify from './Spotify';
+import searchEnterTrigger from './SearchTrigger';
+import selectAlbumTrigger from './SelectAlbumTrigger';
+import playlistTrigger from './PlaylistTrigger';
 
-import renderAlbums from './AlbumList';
-
-const albums = spotify.search.albums('MC Kevinho');
-const element = document.getElementById("album-list");
-albums.then(data => renderAlbums(data.albums.items, element));
+searchEnterTrigger();
+selectAlbumTrigger();
+playlistTrigger();
